0068b77983c468c07f97c7f1b8e957aa99f4d516
Zach White <skullydazed@gmail.com>
2021-01-25 19:12:43 -0800

    changes to make it work on ootb debian

--- install.sh
+++ install.sh
@@ -24,6 +24,48 @@ if ! git --version &> /dev/null; then
 
 fi
 
+# Make sure python is installed
+if ! python3 --version &> /dev/null; then
+	case $(uname) in
+		Linux)
+			if grep -q 'ID.*debian' /etc/os-release; then
+				echo 'Python3 not found! Installing python3!'
+				sudo apt update
+				sudo apt install python3
+			else
+				echo 'Unknown Linux distro! Install python3!'
+				exit 1
+			fi
+		;;
+		*)
+			echo 'Unknown OS! Install python3!'
+			exit 1
+		;;
+	esac
+
+fi
+
+# Make sure python-pip is installed
+if ! pip3 --version &> /dev/null; then
+	case $(uname) in
+		Linux)
+			if grep -q 'ID.*debian' /etc/os-release; then
+				echo 'pip3 not found! Installing pip3!'
+				sudo apt update
+				sudo apt install pip3
+			else
+				echo 'Unknown Linux distro! Install pip3!'
+				exit 1
+			fi
+		;;
+		*)
+			echo 'Unknown OS! Install pip3!'
+			exit 1
+		;;
+	esac
+
+fi
+
 # Fetch our dotfiles
 if ! [ -d ~/dotfiles ]; then
 	(cd ~ && git clone https://github.com/skullydazed/dotfiles.git) || exit
@@ -36,8 +78,8 @@ if ! which thefuck 2>&1 > /dev/null; then
 	case $(uname) in
 		Linux)
 			if grep -q 'ID.*debian' /etc/os-release; then
-				apt update
-				apt install thefuck
+				sudo apt update
+				sudo apt install thefuck
 			else
 				pip3 install thefuck
 			fi
