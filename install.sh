40d51e3ff831998a5d7ac1c2d98d94d1080e5cf7
Zach White <skullydazed@gmail.com>
2021-01-25 19:08:43 -0800

    install git if it's not installed

--- install.sh
+++ install.sh
@@ -3,6 +3,27 @@
 # Make sure we start from the user's home directory
 cd ~
 
+# Make sure git is installed
+if ! git --version &> /dev/null; then
+	case $(uname) in
+		Linux)
+			if grep -q 'ID.*debian' /etc/os-release; then
+				echo 'Git not found! Installing git!'
+				sudo apt update
+				sudo apt install git
+			else
+				echo 'Unknown Linux distro! Install git!'
+				exit 1
+			fi
+		;;
+		*)
+			echo 'Unknown OS! Install git!'
+			exit 1
+		;;
+	esac
+
+fi
+
 # Fetch our dotfiles
 if ! [ -d ~/dotfiles ]; then
 	(cd ~ && git clone https://github.com/skullydazed/dotfiles.git) || exit
@@ -14,8 +35,7 @@ fi
 if ! which thefuck 2>&1 > /dev/null; then
 	case $(uname) in
 		Linux)
-			source /etc/os-release
-			if [ $ID = debian -o $ID_LIKE = debian ]; then
+			if grep -q 'ID.*debian' /etc/os-release; then
 				apt update
 				apt install thefuck
 			else
